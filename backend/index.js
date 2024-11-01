d3d5ad1b57e41ac62b1a7503e825f89529c9a982
tranhuuloc <tranhuuloc4423@gmail.com>
2024-11-01 21:56:08 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ const app = express()
 app.use(
   cors({
     origin: '*',
-    methods: 'GET,POST,PUT,DELETE'
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: false
   })
 )
 app.use(express.json({ limit: '100mb' }))
