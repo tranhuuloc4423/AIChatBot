914ec38038701acb22c0e1b68f6518c764b8bff7
tranhuuloc <tranhuuloc4423@gmail.com>
2024-11-01 21:50:30 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ import { verifyToken } from './controllers/authControllers.js'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE'
+  })
+)
 app.use(express.json({ limit: '100mb' }))
 
 app.use('/auth', authRoutes)
--- frontend/axiosInstance.js
+++ frontend/axiosInstance.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 const instance = axios.create({
   // baseURL: 'http://172.16.65.29:3000',
   baseURL: 'https://aichatbot-smoky.vercel.app',
+  // baseURL:
+  //   'https://aichatbot-git-master-tranhuuloc4423s-projects.vercel.app/?vercelToolbarCode=zRFiyvtTt_uS9UV',
   headers: {
     'Content-Type': 'application/json'
   }
